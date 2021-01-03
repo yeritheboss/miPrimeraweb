bcf1c2d461312376bf5643838307d6764aeef73a
yeritheboss <56045004+yeritheboss@users.noreply.github.com>
2021-01-03 16:08:05 +0100

    ajustes de bordes

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,17 @@
 //toggle menu/bar script
 
 $(document).ready(function (){
+ //funcion sticky del navbar
+    $(window).scroll(function (){
+        if (this.scrollY>20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        })
 
-
-
-    //funcion click menu
-   $('.menu-btn').click(function(){
+       //funcion click menu
+      $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
 
