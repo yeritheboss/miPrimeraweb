aa4c3f8566150e1666a7c9d577de3d83b9e29f11
yeritheboss <56045004+yeritheboss@users.noreply.github.com>
2021-02-15 20:48:30 +0100

    nueva seccion

--- static/js/script.js
+++ static/js/script.js
@@ -1,37 +1,53 @@
-$(document).ready(function (){
- //funcion sticky del navbar
-    $(window).scroll(function (){
-        if (this.scrollY>20){
+$(document).ready(function () {
+    //funcion sticky del navbar
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
-        });
+    });
 
-       //funcion click menu
-      $('.menu-btn').click(function(){
+    //funcion click menu
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
 
 
-        });
+    });
 
 
 
-        //Typed animation
-        var typed= new Typed(".texto",{
-        strings:["Java Programmer","Ingeniero de comunicaciones",
-        "Desarrollador web", "Data Scientist"],
-        typespeed:100,
-        backspeed:60,
-        loop:true
-        });
-        
-        
-        $('.carousel').owlCarousel({   
-    
-            });
+    //Typed animation
+    var typed = new Typed(".texto", {
+        strings: ["Java Programmer", "Ingeniero de comunicaciones",
+            "Desarrollador web", "Data Scientist"
+        ],
+        typespeed: 100,
+        backspeed: 60,
+        loop: true
+    });
+
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+    });
 });
-
-
-
