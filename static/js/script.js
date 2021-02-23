2321b991b9ae420b2508c58022d659869ff23c57
Paul <paulx6_2006@hotmail.com>
2021-02-23 10:41:37 +0100

    Fix sendMail function

--- static/js/script.js
+++ static/js/script.js
@@ -1,77 +1,70 @@
 $(document).ready(function () {
-    //funcion sticky del navbar
-    $(window).scroll(function () {
-        if (this.scrollY > 20) {
-            $('.navbar').addClass("sticky");
-        } else {
-            $('.navbar').removeClass("sticky");
-        }
-    });
-
-    //funcion click menu
-    $('.menu-btn').click(function () {
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-
-
-    });
-
-
-
-    //Typed animation
-    var typed = new Typed(".texto", {
-        strings: ["Java Programmer", "Ingeniero de comunicaciones",
-            "Desarrollador web", "Data Scientist"
-        ],
-        typeSpeed: 100,
-        backSpeed: 80,
-        loop: true
-    });
-
-
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0: {
-                items: 1,
-                nav: false
-            },
-            600: {
-                items: 2,
-                nav: false
-            },
-            1000: {
-                items: 3,
-                nav: false
-            }
-        }
-    });
-
-    setTimeout(function () {
-      $('.center').fadeToggle();
-    }, 1500);
-
-   
-    function sendMail(params){
-       
-        var tempParams={
-            from_name: document.getElementById("fromName").value,
-            from_email:document.getElementById("fromEmail").value,
-            subject:document.getElementById("subject").value,
-            message: document.getElementById("msg").value,
-        };
-
-        emailjs.send('service_jam5heg','template_cynocgd', tempParams)
-        .then(function(res){
-            console.log("success", res.status);
-        })
-
+  //funcion sticky del navbar
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
     }
+  });
 
+  //funcion click menu
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
 
-   
+  //Typed animation
+  var typed = new Typed(".texto", {
+    strings: [
+      "Java Programmer",
+      "Ingeniero de comunicaciones",
+      "Desarrollador web",
+      "Data Scientist",
+    ],
+    typeSpeed: 100,
+    backSpeed: 80,
+    loop: true,
+  });
+
+  // owl carousel script
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
+
+  setTimeout(function () {
+    $(".center").fadeToggle();
+  }, 1500);
 });
+
+function sendMail(params) {
+  var tempParams = {
+    from_name: document.getElementById("fromName").value,
+    from_email: document.getElementById("fromEmail").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("msg").value,
+  };
+
+  emailjs
+    .send("service_jam5heg", "template_cynocgd", tempParams)
+    .then(function (res) {
+      console.log("success", res.status);
+    });
+}
