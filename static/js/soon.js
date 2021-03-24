ec9b0861d0b57b5709a99cef24bd07e5f6a159a3
yeritheboss <56045004+yeritheboss@users.noreply.github.com>
2021-03-24 10:30:24 +0100

    sds

--- static/js/soon.js
+++ static/js/soon.js
@@ -26,3 +26,17 @@ var x = setInterval(function() {
     document.getElementById("demo").innerHTML = "EXPIRED";
   }
 }, 1000);
+
+
+
+var i = 0;
+var txt = 'Lorem ipsum typing effect!'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("type").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
